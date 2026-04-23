3f7f0b1398babb2a75dd05ab9f8732b6f2b365cf
sonhayoni <qheodus123@gmail.com>
2026-04-23 11:15:04 +0900

    Initial commit

---
+++ src/main/java/com/ShipmentSystemApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmentSystemApplication.class, args);
+    }
+}
